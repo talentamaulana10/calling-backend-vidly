ea7d4573de94d2e9bd48dfc7839b537ac11bb5e9
talentamaulana10 <tatamf.id@gmail.com>
2019-04-18 11:15:36 +0700

    Saving the Movie

--- Documents/cwm-calling-backend/vidly/src/components/movieForm.jsx
+++ Documents/cwm-calling-backend/vidly/src/components/movieForm.jsx
@@ -42,10 +42,11 @@ class MovieForm extends Form {
   }
   async populateMovies() {
     try {
-      const { data: movie } = await getMovie(movieId);
-      this.setState({ data: this.mapToViewModel(movie) });
       const movieId = this.props.match.params.id;
       if (movieId === "new") return;
+
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
@@ -67,8 +68,8 @@ class MovieForm extends Form {
     };
   }
 
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
 
     this.props.history.push("/movies");
   };
